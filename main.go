1f1812633e542ea794e59423794b0e0aa8fa9845
Jinsung Ha <jsung5381@naver.com>
2022-06-21 00:55:09 +0900

    Add simple main script

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type TrackParcelPayload struct {
+	TKey     string `json:"t_key"`
+	TCode    string `json:"t_code"`
+	TInvoice string `json:"t_invoice"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	payload := TrackParcelPayload{
+		TKey:     "wS6QdxNImv6DaPt0or1X4g",
+		TCode:    "04",
+		TInvoice: "648428990916",
+	}
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest(
+		"GET",
+		"http://info.sweettracker.co.kr"+"/api/v1/trackingInfo",
+		bytes.NewBuffer(jsonPayload),
+	)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	var data interface{}
+	json.NewDecoder(resp.Body).Decode(&data)
+	fmt.Println(data)
 }
