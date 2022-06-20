3851c203f09db6633818707d3c29dff313133af6
Jinsung Ha <jsung5381@naver.com>
2022-06-21 01:02:28 +0900

    Add payload and resp schema, resp schema needs to be refactored

--- client/schema.go
+++ client/schema.go
@@ -1,1 +1,13 @@
 package client
+
+type TrackParcelPayload struct {
+	TKey     string `json:"t_key"`
+	TCode    string `json:"t_code"`
+	TInvoice string `json:"t_invoice"`
+}
+
+type TrackParcelResp struct {
+	Complete  bool   `json:"complete"`
+	Estimate  string `json:"estimate"`
+	InvoiceNo string `json:"invoiceNo"`
+}
